ecb3fafdb31ec302785762a37bc1c3423fded518
Rioreyblue <franciscorey702@gmail.com>
2026-03-10 23:13:23 +0800

    NoteApp

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:energy_apk/homepage.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      //
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/',
+
+      routes: {
+        '/': (context)=> Homepage()
+      },
+    );
+  }
+}
