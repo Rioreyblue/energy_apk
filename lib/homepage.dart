b4b432b26fa5f2b0786d93757f838e0354c35f11
Rioreyblue <franciscorey702@gmail.com>
2026-03-12 00:08:29 +0800

    fix_bugs

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -9,30 +9,30 @@ class Homepage extends StatefulWidget {
 
 class _HomepageState extends State<Homepage> {
   int _currentIndex = 0;
+
   final List<String> _labels = [
     'ENERGY SMART',
     'MONITORING',
     'GOALS',
     'SETTINGS',
   ];
+
   late final List<Widget> _pages;
 
   @override
   void initState() {
     super.initState();
-    // Initialize pages with callback for HomePage
     _pages = [
-      Center(child: Text('Home Page')),
-      Center(child: Text('Monitoring Page')),
-      Center(child: Text('Goals Page')),
-      Center(child: Text('Settings Page')),
+      const Center(child: Text('Home Page')),
+      const Center(child: Text('Monitoring Page')),
+      const Center(child: Text('Goals Page')),
+      const Center(child: Text('Settings Page')),
     ];
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      //appBar
       appBar: AppBar(
         title: Text(
           _labels[_currentIndex],
@@ -44,16 +44,28 @@ class _HomepageState extends State<Homepage> {
         ),
         backgroundColor: Colors.green,
       ),
-
-      //body
       body: _pages[_currentIndex],
-
-      //bottom navigation
       bottomNavigationBar: BottomNavigationBar(
-        items: [
-          BottomNavigationBarItem(icon: Icon(Icons.home)),
-          BottomNavigationBarItem(icon: Icon(Icons.search)),
-          BottomNavigationBarItem(icon: Icon(Icons.person)),
+        currentIndex: _currentIndex,
+        // Set type to fixed if you have more than 3 items and want labels to always show
+        type: BottomNavigationBarType.fixed,
+        selectedItemColor: Colors.green,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.monitor), label: 'Monitor'),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.flag),
+            label: 'Goals',
+          ), // or Icons.flag
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Settings',
+          ),
         ],
       ),
     );
